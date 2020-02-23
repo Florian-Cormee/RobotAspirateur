df92cc0fa90a7077184f2bfe8059253b41fdd083
Florian _ <florian.cormee@gmail.com>
2020-02-23 14:14:23 +0100

    Implementation des tests pour Vecteur2D

--- test/fr/rob4/simulation/geometrie/Vecteur2DTest.java
+++ test/fr/rob4/simulation/geometrie/Vecteur2DTest.java
@@ -3,6 +3,8 @@ package fr.rob4.simulation.geometrie;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class Vecteur2DTest {
@@ -15,45 +17,93 @@ public class Vecteur2DTest {
 
         a = new Vecteur2D(-0.5, 15);
         Vecteur2D c = b.addition(a);
-        assertEquals(b.getX() + a.getX(), c.getX(), 1e-20);
-        assertEquals(a.getY() + b.getY(), c.getY(), 1e-20);
+        assertEquals(b.getX() + a.getX(), c.getX(), 1e-10);
+        assertEquals(a.getY() + b.getY(), c.getY(), 1e-10);
     }
 
     @Test
     public void angle() {
         Vecteur2D a = new Vecteur2D(0, 1);
-        assertEquals(0, a.angle(a), 1e-20);
+        assertEquals(0, a.angle(a), 1e-10);
 
-        Vecteur2D b = new Vecteur2D();
+        a = new Vecteur2D(1, 0);
+        Vecteur2D b = new Vecteur2D(1, 1);
+        assertEquals(Math.PI / 4, a.angle(b), 1e-10);
+        b = new Vecteur2D(0, -1);
+        assertEquals(-Math.PI / 2, a.angle(b), 1e-10);
+        b = new Vecteur2D(-1, 0);
+        assertEquals(Math.PI, a.angle(b), 1e-10);
+
+        b = new Vecteur2D();
         try {
-            b.angle(a);
-            Assert.fail("Attendait une erreur");
+            double res = b.angle(a);
+            Assert.fail(String.format("Attendait une erreur a obtenu: %f", res));
         } catch (Exception e) {
         }
-        b = new Vecteur2D(1,1);
+
     }
 
     @Test
     public void normalise() {
+        Vecteur2D a = new Vecteur2D(Math.random(), Math.random());
+        Vecteur2D b = a.normalise();
+        assertEquals(0, a.angle(b), 1e-10);
+        assertEquals(1, b.norme(), 1e-10);
+
+        try {
+            new Vecteur2D().normalise();
+            Assert.fail("Attendait une erreur");
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void norme() {
+        Random r = new Random();
+        Vecteur2D a = new Vecteur2D(-r.nextDouble(), r.nextDouble());
+        assertEquals(Math.sqrt(a.getX() * a.getX() + a.getY() * a.getY()), a.norme(), 1e-10);
     }
 
     @Test
     public void produit() {
+        Random r = new Random();
+        double coef = r.nextDouble() + 0.1;
+        Vecteur2D a = new Vecteur2D(-r.nextDouble(), r.nextDouble());
+        Vecteur2D b = a.produit(coef);
+        assertEquals(0, a.angle(b), 1e-10);
+        assertEquals(a.norme() * coef, b.norme(), 1e-10);
     }
 
     @Test
     public void rotation() {
+        double angle = (Math.random() - 0.5) * 2 * Math.PI;
+        Vecteur2D a = new Vecteur2D(1, 0);
+        Vecteur2D b = a.rotation(angle);
+        assertEquals(a.norme(), b.norme(), 1e-10);
+        assertEquals(angle, a.angle(b), 1e-10);
+
+        try {
+            new Vecteur2D().rotation(10);
+            Assert.fail("Attendait une erreur");
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void scalaire() {
+        Random r = new Random();
+        Vecteur2D a = new Vecteur2D(-r.nextDouble(), r.nextDouble());
+        Vecteur2D b = new Vecteur2D(r.nextDouble(), r.nextDouble());
+        assertEquals(a.getX() * b.getX() + a.getY() * b.getY(), a.scalaire(b), 1e-10);
     }
 
     @Test
     public void soustraction() {
+        Random r = new Random();
+        Vecteur2D a = new Vecteur2D(-r.nextDouble(), r.nextDouble());
+        Vecteur2D b = new Vecteur2D(r.nextDouble(), r.nextDouble());
+        Vecteur2D c= a.soustraction(b);
+        assertEquals(a.getX() - b.getX() , c.getX(), 1e-10);
+        assertEquals(a.getY() - b.getY() , c.getY(), 1e-10);
     }
 }
