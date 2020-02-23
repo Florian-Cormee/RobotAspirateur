c3fa00b36aa29352e2d38e2812c013845ee69d19
Florian _ <florian.cormee@gmail.com>
2020-02-23 13:39:58 +0100

    Correction des erreurs de compilation

--- src/fr/rob4/simulation/strategie/StrategieSuiveurMurs.java
+++ src/fr/rob4/simulation/strategie/StrategieSuiveurMurs.java
@@ -1,11 +1,11 @@
 package fr.rob4.simulation.strategie;
 
-import fr.rob4.simulation.element.Robot;
+import fr.rob4.simulation.element.IRobot;
 
 public class StrategieSuiveurMurs implements IStrategie {
 
     @Override
-    public boolean decide(Robot robot) {
+    public boolean decide(IRobot robot) {
 	// TODO Auto-generated method stub
 	return false;
     }
