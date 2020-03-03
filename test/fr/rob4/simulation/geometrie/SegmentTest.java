ab23fc3ad3b20e484432ccb45421c3af54ac6a8a
Florian _ <florian.cormee@gmail.com>
2020-03-03 17:10:56 +0100

    Test d'intersection avec un cercle

--- test/fr/rob4/simulation/geometrie/SegmentTest.java
+++ test/fr/rob4/simulation/geometrie/SegmentTest.java
@@ -1,8 +1,13 @@
 package fr.rob4.simulation.geometrie;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import fr.rob4.simulation.exception.NoIntersectionException;
 
 class SegmentTest {
 
@@ -13,7 +18,12 @@ class SegmentTest {
 
     @Test
     void testGetDimension() {
-	fail("Not yet implemented");
+	Point2D p1 = new Point2D(new Vecteur2D(0, 0));
+	Point2D p2 = new Point2D(new Vecteur2D(1, 0));
+	Segment s = new Segment(p1, p2);
+	Rectangle r = s.getDimension();
+	assertEquals(1, r.getLargeur(), 1e-10);
+	assertEquals(0, r.getHauteur(), 1e-10);
     }
 
     @Test
@@ -30,9 +40,7 @@ class SegmentTest {
 	Point2D p1 = new Point2D(new Vecteur2D(1, 0));
 	Point2D p2 = new Point2D(new Vecteur2D(2, 0));
 	Segment s = new Segment(p1, p2);
-	Segment sr = s.rotation(Math.PI/2, p1);
-	System.out.println(sr.getA());
-	System.out.println(sr.getB());
+	Segment sr = s.rotation(Math.PI / 2, p1);
 	assertEquals(s.getA(), sr.getA());
 	p2 = new Point2D(new Vecteur2D(1, 1));
 	assertEquals(p2, sr.getB());
@@ -40,12 +48,54 @@ class SegmentTest {
 
     @Test
     void testIntersecteSegment() {
-	fail("Not yet implemented");
+	Point2D a = new Point2D(new Vecteur2D(-1, -1));
+	Point2D b = new Point2D(new Vecteur2D(1, 1));
+	Point2D c = new Point2D(new Vecteur2D(-1, 1));
+	Point2D d = new Point2D(new Vecteur2D(1, -1));
+	Segment s1 = new Segment(a, b);
+	Segment s2 = new Segment(c, d);
+	// Intersection de segment en leur milieu
+	try {
+	    Point2D inter = s1.intersecte(s2);
+	    assertEquals(new Point2D(new Vecteur2D()), inter);
+	} catch (NoIntersectionException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	    fail();
+	}
+	// Intersection de segments parallèles
+	s2 = new Segment(a, b);
+	try {
+	    s1.intersecte(s2);
+	    fail("Segment parallèles sans exception");
+	} catch (NoIntersectionException e) {
+	}
+	// Intersection à l'extrémité
+	s2 = new Segment(c, b);
+	try {
+	    Point2D inter = s1.intersecte(s2);
+	    assertEquals(b, inter);
+	} catch (NoIntersectionException e) {
+	    e.printStackTrace();
+	    fail();
+	}
     }
 
     @Test
     void testIntersecteCercle() {
-	fail("Not yet implemented");
+	Cercle cercle = new Cercle(0, 0, 1);
+	Point2D a = new Point2D(new Vecteur2D(1.1, 0));
+	Point2D b = new Point2D(new Vecteur2D(-1.1, 0));
+	Segment s = new Segment(a, b);
+	try {
+	    List<Point2D> l = s.intersecte(cercle);
+	    assertEquals(2, l.size());
+	    System.out.println(l);
+	} catch (NoIntersectionException e) {
+	    e.printStackTrace();
+	    fail();
+	}
+	
     }
 
     @Test
@@ -65,7 +115,12 @@ class SegmentTest {
 
     @Test
     void testNorme() {
-	fail("Not yet implemented");
+	Point2D p1 = new Point2D(new Vecteur2D(1, 0));
+	Point2D p2 = new Point2D(new Vecteur2D(2, 0));
+	Segment s = new Segment(p1, p2);
+
+	double norme = s.norme();
+	assertEquals(1.0, norme, 1e-10);
     }
 
 }
