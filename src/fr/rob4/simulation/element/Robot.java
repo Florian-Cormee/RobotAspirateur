aa54bca03a5dcc00f6404a2f0337e30bf2357c74
Florian Cormée <florian.cormee@gmail.com>
2020-03-05 15:47:27 +0100

    Ajout d'un getter pour l'orientation

--- src/fr/rob4/simulation/element/Robot.java
+++ src/fr/rob4/simulation/element/Robot.java
@@ -36,21 +36,18 @@ public class Robot extends Element implements IRobot {
      *
      * @see Collections#emptyList()
      */
-    public Robot(Forme forme,
-                 Collection<? extends IModule<?>> capteurs,
-                 IStrategie strategie,
-                 double theta,
-                 double ecartRoues) {
-        super(forme);
-        this.strategie = Objects.requireNonNull(strategie);
-        this.theta = theta;
-        if (capteurs == null) {
-            this.modules = Collections.emptyList();
-        } else {
-            this.modules = new ArrayList<>(capteurs);
-        }
-        this.ecartRoues = ecartRoues;
-        nettoie = false;
+    public Robot(Forme forme, Collection<? extends IModule<?>> capteurs,
+	    IStrategie strategie, double theta, double ecartRoues) {
+	super(forme);
+	this.strategie = Objects.requireNonNull(strategie);
+	this.theta = theta;
+	if (capteurs == null) {
+	    this.modules = Collections.emptyList();
+	} else {
+	    this.modules = new ArrayList<>(capteurs);
+	}
+	this.ecartRoues = ecartRoues;
+	nettoie = false;
     }
 
     /**
@@ -58,47 +55,48 @@ public class Robot extends Element implements IRobot {
      */
     @Override
     public void actualise(Simulation simulation) {
-        for (IModule<?> module : modules) {
-            module.actualise(simulation);
-        }
-        strategie.decide(this);
+	for (IModule<?> module : modules) {
+	    module.actualise(simulation);
+	}
+	strategie.decide(this);
     }
 
     @Override
     public void deplace(double dG, double dD) {
-        // Limitation du déplacement des roues
-        dG = Math.min(dG, VITESSE_MAX * Simulation.T);
-        dD = Math.min(dD, VITESSE_MAX * Simulation.T);
-        // Déplacement du robot (cf. code fourni dans Position.java)
-        double alpha = (dD - dG) / ecartRoues;
-        Vecteur2D deplacement;
-        if (Math.abs(alpha) > 1e-20) {
-            // Rotation non négligeable
-            double r = (dG / alpha) + (ecartRoues / 2);
-            double dx = r * (Math.cos(alpha) - 1);
-            double dy = r * Math.sin(alpha);
-            deplacement = new Vecteur2D(dx, dy);
-            deplacement = deplacement.rotation(theta - Math.PI / 2);
-        } else {
-            // Rotation négligeable
-            double dx = dG * Math.cos(theta);
-            double dy = dG * Math.sin(theta);
-            deplacement = new Vecteur2D(dx, dy);
-            alpha = 0;
-        }
-        // Mise à jour de la position et de l'orientation
-        Point2D pos = getPosition().deplace(deplacement);
-        setPosition(pos);
-        theta += alpha;
+	// Limitation du déplacement des roues
+	dG = Math.min(dG, VITESSE_MAX * Simulation.T);
+	dD = Math.min(dD, VITESSE_MAX * Simulation.T);
+	// Déplacement du robot (cf. code fourni dans Position.java)
+	double alpha = (dD - dG) / ecartRoues;
+	Vecteur2D deplacement;
+	if (Math.abs(alpha) > 1e-20) {
+	    // Rotation non négligeable
+	    double r = (dG / alpha) + (ecartRoues / 2);
+	    double dx = r * (Math.cos(alpha) - 1);
+	    double dy = r * Math.sin(alpha);
+	    deplacement = new Vecteur2D(dx, dy);
+	    deplacement = deplacement.rotation(theta - Math.PI / 2);
+	} else {
+	    // Rotation négligeable
+	    double dx = dG * Math.cos(theta);
+	    double dy = dG * Math.sin(theta);
+	    deplacement = new Vecteur2D(dx, dy);
+	    alpha = 0;
+	}
+	// Mise à jour de la position et de l'orientation
+	Point2D pos = getPosition().deplace(deplacement);
+	setPosition(pos);
+	theta += alpha;
     }
 
-    /**
-     * Obtient la postion
-     *
-     * @return La position
-     */
+    @Override
     public Point2D getPosition() {
-        return forme.getCentre();
+	return forme.getCentre();
+    }
+
+    @Override
+    public double getOrientation() {
+	return theta;
     }
 
     /**
@@ -110,50 +108,51 @@ public class Robot extends Element implements IRobot {
      * @param pos La nouvelle position
      */
     protected void setPosition(Point2D pos) {
-        Objects.requireNonNull(pos);
-        dernierePos = getPosition();
-        forme.setCentre(pos);
+	Objects.requireNonNull(pos);
+	dernierePos = getPosition();
+	forme.setCentre(pos);
     }
 
     @Override
     public List<IModule<?>> getModules() {
-        return Collections.unmodifiableList(modules);
+	return Collections.unmodifiableList(modules);
     }
 
     @Override
     public <T extends IModule<?>> List<T> getModules(Class<T> c) {
-        List<T> list = new ArrayList<>();
-        for (IModule<?> module : modules) {
-            if (c.isInstance(module)) {
-                list.add(c.cast(module));
-            }
-        }
-        return list;
+	List<T> list = new ArrayList<>();
+	for (IModule<?> module : modules) {
+	    if (c.isInstance(module)) {
+		list.add(c.cast(module));
+	    }
+	}
+	return list;
     }
 
     @Override
     public IStrategie getStrategie() {
-        return strategie;
+	return strategie;
     }
 
     @Override
     public boolean isNettoie() {
-        return nettoie;
+	return nettoie;
     }
 
     @Override
     public void setNettoie(boolean etat) {
-        nettoie = etat;
+	nettoie = etat;
     }
 
     @Override
     public void gereCollision(ICollisionable element) {
-        // Annulation du dernier déplacement
-        setPosition(dernierePos);
+	// Annulation du dernier déplacement
+	setPosition(dernierePos);
     }
 
     @Override
     public String toString() {
-        return "Robot [modules=" + modules + ", strategie=" + strategie + ", theta=" + theta + ", toString()=" + super.toString() + "]";
+	return "Robot [modules=" + modules + ", strategie=" + strategie
+		+ ", theta=" + theta + ", toString()=" + super.toString() + "]";
     }
 }
