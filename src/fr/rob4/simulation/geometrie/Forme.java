7271c95801242bfe664dbe6babfc3c61c270c4c3
Florian _ <florian.cormee@gmail.com>
2020-03-07 14:36:41 +0100

    Correction de rotation dans arc de cercle & nettoyage

--- src/fr/rob4/simulation/geometrie/Forme.java
+++ src/fr/rob4/simulation/geometrie/Forme.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package fr.rob4.simulation.geometrie;
 
@@ -8,7 +8,7 @@ import fr.rob4.simulation.exception.NoIntersectionException;
 /**
  * Cette classe abstraite est une forme géometrique. Attribut de tous les
  * elements de la simulation.
- * 
+ *
  * @author Florentin BEROUJON & Florian CORMEE
  * @version 0.0.1
  * @see Point2D
@@ -20,83 +20,83 @@ import fr.rob4.simulation.exception.NoIntersectionException;
  */
 public abstract class Forme {
 
-	// Attribut
-	protected Point2D centre;
+    // Attribut
+    protected Point2D centre;
 
-	/**
-	 * Crée une Forme à partir de son centre qui est un Point2D.
-	 * 
-	 * @param p Le point qui deviendra le centre de la forme.
-	 */
-	public Forme(Point2D p) {
-		centre = p;
-	}
+    /**
+     * Crée une Forme à partir de son centre qui est un Point2D.
+     *
+     * @param p Le point qui deviendra le centre de la forme.
+     */
+    public Forme(Point2D p) {
+		this.centre = p;
+    }
 
-	/**
-	 * Crée une Forme à partir des coordonnées de son centre.
-	 * 
-	 * @param x Abscisse du centre.
-	 * @param y Ordonnée du centre.
-	 */
-	public Forme(double x, double y) {
-		centre = new Point2D(new Vecteur2D(x, y));
-	}
+    /**
+     * Crée une Forme à partir des coordonnées de son centre.
+     *
+     * @param x Abscisse du centre.
+     * @param y Ordonnée du centre.
+     */
+    public Forme(double x, double y) {
+		this.centre = new Point2D(new Vecteur2D(x, y));
+    }
 
-	/**
-	 * Obtient le centre.
-	 * 
-	 * @return Le centre (Point2D)
-	 */
-	public Point2D getCentre() {
-		return centre;
-	}
+    /**
+     * Permet de savoir la Forme est supperposée avec l'autre forme {@code f}.
+     *
+     * @param f Forme avec laquelle il fut tester la superposition.
+     * @return true si les formes sont superposées, false sinon.
+     */
+    public abstract boolean collisionne(Forme f) throws NoIntersectionException;
 
-	/**
-	 * Modifie le centre.
-	 * 
-	 * @param centre Le nouveau centre.
-	 */
-	public void setCentre(Point2D centre) {
-		this.centre = centre;
-	}
+    /**
+     * Obtient le centre.
+     *
+     * @return Le centre (Point2D)
+     */
+    public Point2D getCentre() {
+        return this.centre;
+    }
 
-	/**
-	 * Permet de savoir la Forme est supperposée avec l'autre forme {@code f}.
-	 * 
-	 * @param f Forme avec laquelle il fut tester la superposition.
-	 * @return true si les formes sont superposées, false sinon.
-	 */
-	public abstract boolean collisionne(Forme f) throws NoIntersectionException;
+    /**
+     * Modifie le centre.
+     *
+     * @param centre Le nouveau centre.
+     */
+    public void setCentre(Point2D centre) {
+        this.centre = centre;
+    }
 
-	/**
-	 * Obtient une nouvelle forme identique à l'initiale tournée de alpha (en
-	 * radian).
-	 * <p>
-	 * Note: L'instance n'est pas modifiée.
-	 * 
-	 * @param alpha Angle de rotation en radian.
-	 * @return La forme tournée de alpha dans une nouvelle instance.
-	 */
-	public Forme rotation(double alpha) {
-		return rotation(alpha, centre);
-	}
+    /**
+     * Obtient le Rectangle encadrant la forme.
+     *
+     * @return Rectangle encadrant la forme.
+     */
+    public abstract Rectangle getDimension();
 
-	/**
-	 * Obtient une nouvelle forme identique à l'originale tournée de alpha (en
-	 * radian) autour du point2D p.
-	 * <p>
-	 * Note: L'instance n'est pas modifiée.
-	 * 
-	 * @param alpha Angle de rotation en radian.
-	 * @param p     Point2D centre de la rotation.
-	 * @return Résultat de la rotation dans une nouvelle instance.
-	 */
-	public abstract Forme rotation(double alpha, Point2D p);
+    /**
+     * Obtient une nouvelle forme identique à l'initiale tournée de alpha (en
+     * radian).
+     * <p>
+     * Note: L'instance n'est pas modifiée.
+     *
+     * @param alpha Angle de rotation en radian.
+     * @return La forme tournée de alpha dans une nouvelle instance.
+     */
+    public Forme rotation(double alpha) {
+        return this.rotation(alpha, this.centre);
+    }
 
-	/**
-	 * Obtient le Rectangle encadrant la forme.
-	 * 
-	 * @return Rectangle encadrant la forme.
-	 */
-	public abstract Rectangle getDimension();
+    /**
+     * Obtient une nouvelle forme identique à l'originale tournée de alpha (en
+     * radian) autour du point2D p.
+     * <p>
+     * Note: L'instance n'est pas modifiée.
+     *
+     * @param alpha Angle de rotation en radian.
+     * @param p     Point2D centre de la rotation.
+     * @return Résultat de la rotation dans une nouvelle instance.
+     */
+    public abstract Forme rotation(double alpha, Point2D p);
 }
