bd9988aeb90e4990f759ecae5fc9b8f4d7456d02
Florian _ <florian.cormee@gmail.com>
2020-03-08 17:11:39 +0100

    Ajout d'un obstacle polygonal

--- src/fr/rob4/simulation/Main.java
+++ src/fr/rob4/simulation/Main.java
@@ -1,11 +1,15 @@
 package fr.rob4.simulation;
 
+import fr.rob4.simulation.geometrie.Point2D;
+import fr.rob4.simulation.geometrie.Polygone;
 import fr.rob4.simulation.geometrie.Rectangle;
+import fr.rob4.simulation.geometrie.Vecteur2D;
 import fr.rob4.simulation.strategie.StrategieAleatoire;
 import fr.rob4.simulation.vue.SimulationPanel;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import java.util.Arrays;
 
 public final class Main {
     private Main() {
@@ -22,7 +26,12 @@ public final class Main {
         builder.ajouteTacheCirculaire(0, -1, 0.2);
         builder.ajouteTacheCirculaire(1, 0, 0.3);
         builder.ajouteElement(factory.tache(new Rectangle(-1, 0, 0.25, 0.15)));
-        builder.ajouteRobotStandard(0, 0, 0, new StrategieAleatoire());
+        Polygone polygone = new Polygone(Arrays.asList(new Point2D(new Vecteur2D(0, 0)),
+                                                       new Point2D(new Vecteur2D(0.4, 0.2)),
+                                                       new Point2D(new Vecteur2D(0.3, 0)),
+                                                       new Point2D(new Vecteur2D(0.4, -0.2))));
+        builder.ajouteElement(factory.obstacle(polygone));
+        builder.ajouteRobotStandard(0, -1, 0, new StrategieAleatoire());
         Simulation s = builder.build();
         SimulationPanel dessinateur = new SimulationPanel(s);
 
