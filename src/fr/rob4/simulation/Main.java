4626072408e3ce7a6e08c1f468e298c0765a2415
Florian _ <florian.cormee@gmail.com>
2020-03-08 16:25:07 +0100

    Simulation nettoyage brique lego

--- src/fr/rob4/simulation/Main.java
+++ src/fr/rob4/simulation/Main.java
@@ -1,11 +1,11 @@
 package fr.rob4.simulation;
 
+import fr.rob4.simulation.geometrie.Rectangle;
 import fr.rob4.simulation.strategie.StrategieAleatoire;
 import fr.rob4.simulation.vue.SimulationPanel;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
-import java.util.Random;
 
 public final class Main {
     private Main() {
@@ -14,14 +14,22 @@ public final class Main {
     public static void main(String[] args) {
         ElementFactory factory = ElementFactory.getInstance();
         SimulationStdBuilder builder = new SimulationStdBuilder(factory);
-		builder.ajouteRobotStandard(0, 0, 0, new StrategieAleatoire());
-		Simulation s = builder.build();
+        builder.ajouteObstacleCirculaire(1, 1, 1);
+        builder.ajouteObstacleCirculaire(-1, 1, 1);
+        builder.ajouteObstacleCirculaire(1, -1, 1);
+        builder.ajouteObstacleCirculaire(-1, -1, 1);
+        builder.ajouteTacheCirculaire(0, 1, 0.3);
+        builder.ajouteTacheCirculaire(0, -1, 0.2);
+        builder.ajouteTacheCirculaire(1, 0, 0.3);
+        builder.ajouteElement(factory.tache(new Rectangle(-1, 0, 0.25, 0.15)));
+        builder.ajouteRobotStandard(0, 0, 0, new StrategieAleatoire());
+        Simulation s = builder.build();
         SimulationPanel dessinateur = new SimulationPanel(s);
 
         JFrame frame = new JFrame("Simulation");
         frame.add(dessinateur);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(250, 250);
+        frame.setSize(1080, 1080);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
